a094e50f1ac86959d96dcac0954cb63a9c747617
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-24 13:58:05 +0900

    기본 프로젝트 세팅 SpringConfiguration.java UserMapper.java(interface) resources/mapper/UserMapper.xml root-context.xml & servlet-context.xml & web.xml

---
+++ src/main/java/com/eathub/conf/SpringConfiguration.java
@@ -0,0 +1,76 @@
+package com.eathub.conf;
+
+import com.eathub.mapper.UserMapper;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@PropertySource("classpath:spring/db.properties")
+@EnableTransactionManagement
+public class SpringConfiguration {
+
+    @Value("${jdbc.driver}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public BasicDataSource dataSource(){
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName(driverClassName);
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
+        sqlSessionFactoryBean.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("mapper/*.xml")
+        );
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    // Mapper 인터페이스를 스프링 빈으로 등록하기 위한 mapperFactoryBean 메소드
+    public <T> MapperFactoryBean<T> mapperFactoryBean(Class<T> mapperInterface) throws Exception {
+        MapperFactoryBean<T> factoryBean = new MapperFactoryBean<>(mapperInterface);
+        factoryBean.setSqlSessionFactory(sqlSessionFactory());
+        return factoryBean;
+    }
+
+//    Mapper 추가시 아래와 같이 추가
+//    @Bean
+//    public MapperFactoryBean<UserMapper> userMapper() throws Exception {
+//        return mapperFactoryBean(UserMapper.class);
+//    }
+}
