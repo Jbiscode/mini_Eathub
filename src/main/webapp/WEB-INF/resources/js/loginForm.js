fd8f96ca80c540e3eb8b82e82153100b6c97bb41
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-28 00:11:06 +0900

    ✨FEAT : 로그인 및 회원가입 관련 화면 추가

---
+++ src/main/webapp/WEB-INF/resources/js/loginForm.js
@@ -0,0 +1,7 @@
+/**
+ * loginForm.html
+ * views/members/loginForm.html
+ */
+document.getElementById('submitButton').addEventListener('click', function() {
+    document.getElementById('loginForm').submit();
+});
