c45df56ceb689014c8d87fe16bbebad017242040
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-27 23:55:38 +0900

    🎨FIX :  "SpringConfiguration 클래스에 DB 설정, MyBatis 설정, 트랜잭션 관리, 메시지 소스 설정 추가" * properties파일의 encoding을 UTF-8로 수정 * MapperScan으로 Mybatis 연결 * Mapper의존성 직접 주입한거 삭제

--- src/main/java/com/eathub/conf/SpringConfiguration.java
+++ src/main/java/com/eathub/conf/SpringConfiguration.java
@@ -1,75 +1,95 @@
 package com.eathub.conf;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@PropertySource("classpath:spring/db.properties")
 @EnableTransactionManagement
+@RequiredArgsConstructor
+@MapperScan("com.eathub.mapper")
 public class SpringConfiguration {
+  private final ApplicationContext applicationContext;
 
-    @Value("${jdbc.driver}")
-    private String driverClassName;
-    @Value("${jdbc.url}")
-    private String url;
-    @Value("${jdbc.username}")
-    private String username;
-    @Value("${jdbc.password}")
-    private String password;
+  @Value("${jdbc.driver}")
+  private String driverClassName;
 
-    @Bean
-    public BasicDataSource dataSource(){
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName(driverClassName);
-        basicDataSource.setUrl(url);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+  @Value("${jdbc.url}")
+  private String url;
 
-        return basicDataSource;
-    }
+  @Value("${jdbc.username}")
+  private String username;
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
-        sqlSessionFactoryBean.setMapperLocations(
-                new PathMatchingResourcePatternResolver().getResources("mapper/*.xml")
-        );
-        return sqlSessionFactoryBean.getObject();
-    }
+  @Value("${jdbc.password}")
+  private String password;
 
-    @Bean
-    public SqlSessionTemplate sqlSession() throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory());
-    }
+  /**
+   * DB설정 파일 읽는 Bean 생성.
+   * 이 메소드는 UTF-8 인코딩으로 'spring/db.properties' 파일을 읽어들임.
+   * 기존에 사용하던 PropertySource 는 인코딩을 지정할 수 없어서 대체.
+   * @return PropertySourcesPlaceholderConfigurer 인스턴스를 반환.
+   */
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer properties() {
+    PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+    configurer.setLocation(new ClassPathResource("spring/db.properties"));
+    configurer.setFileEncoding("UTF-8");
+    return configurer;
+  }
 
-    @Bean
-    public DataSourceTransactionManager transactionManager(){
-        return new DataSourceTransactionManager(dataSource());
-    }
+  @Bean
+  public BasicDataSource dataSource() {
+    BasicDataSource basicDataSource = new BasicDataSource();
+    basicDataSource.setDriverClassName(driverClassName);
+    basicDataSource.setUrl(url);
+    basicDataSource.setUsername(username);
+    basicDataSource.setPassword(password);
 
-    // Mapper 인터페이스를 스프링 빈으로 등록하기 위한 mapperFactoryBean 메소드
-    public <T> MapperFactoryBean<T> mapperFactoryBean(Class<T> mapperInterface) throws Exception {
-        MapperFactoryBean<T> factoryBean = new MapperFactoryBean<>(mapperInterface);
-        factoryBean.setSqlSessionFactory(sqlSessionFactory());
-        return factoryBean;
-    }
+    return basicDataSource;
+  }
 
-//    Mapper 추가시 아래와 같이 추가
-//    @Bean
-//    public MapperFactoryBean<MemberMapper> userMapper() throws Exception {
-//        return mapperFactoryBean(MemberMapper.class);
-//    }
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+    sqlSessionFactoryBean.setDataSource(dataSource());
+    sqlSessionFactoryBean.setConfigLocation(
+      new ClassPathResource("spring/mybatis-config.xml")
+    );
+    sqlSessionFactoryBean.setMapperLocations(
+      applicationContext.getResources("classpath:mapper/*Mapper.xml")
+    );
+    return sqlSessionFactoryBean.getObject();
+  }
+
+  @Bean
+  public SqlSessionTemplate sqlSession() throws Exception {
+    return new SqlSessionTemplate(sqlSessionFactory());
+  }
+
+  @Bean
+  public DataSourceTransactionManager transactionManager() {
+    return new DataSourceTransactionManager(dataSource());
+  }
+
+//  예외메시지 처리를 위한 MessageSource Bean 설정
+  @Bean
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
 }
