9e330b67b2dcbf9765deb08aad69f55e45e3e11f
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-03-31 11:44:37 +0900

    :recycle: Members Entity 추가 & DTO역할 분리

--- src/main/java/com/eathub/service/MemberService.java
+++ src/main/java/com/eathub/service/MemberService.java
@@ -1,20 +1,22 @@
 package com.eathub.service;
 
 import com.eathub.dto.LoginDTO;
-import com.eathub.dto.MemberDTO;
+import com.eathub.dto.MemberJoinDTO;
+import com.eathub.entity.Members;
 import com.eathub.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberMapper memberMapper;
 
-    public MemberDTO selectMemberById(String member_id) {
+    public Members selectMemberById(String member_id) {
         return memberMapper.selectMemberById(member_id);
     }
 
@@ -22,34 +24,58 @@ public class MemberService {
         return memberMapper.selectMemberById(member_id) != null;
     }
 
-    public void insertMember(MemberDTO memberDTO) {
-        memberMapper.insertMember(memberDTO);
+    public void insertMember(Members memberJoinDTO) {
+        memberMapper.insertMember(memberJoinDTO);
     }
 
-    public MemberDTO login(LoginDTO loginDTO, BindingResult bindingResult) {
+    public Members login(LoginDTO loginDTO, BindingResult bindingResult) {
+        if (validateLoginInputs(loginDTO, bindingResult)) {
+            return null;
+        }
+        return processLogin(loginDTO, bindingResult);
+    }
+
+
+    public void updateMember(Members memberUpdateDTO) {
+        memberMapper.updateMember(memberUpdateDTO);
+    }
+
+    private boolean validateLoginInputs(LoginDTO loginDTO, BindingResult bindingResult) {
         if (!StringUtils.hasText(loginDTO.getMember_id())) {
             bindingResult.rejectValue("member_id", "required");
         }
         if (!StringUtils.hasText(loginDTO.getMember_pwd())) {
             bindingResult.rejectValue("member_pwd", "required");
         }
-
-        if (bindingResult.hasErrors()) {
-            return null;
-        }
-
-        MemberDTO loginMember = memberMapper.login(loginDTO);
-
+        return bindingResult.hasErrors();
+    }
+    private Members processLogin(LoginDTO loginDTO, BindingResult bindingResult) {
+        Members loginMember = memberMapper.login(loginDTO);
         if (loginMember == null) {
             bindingResult.rejectValue("member_pwd", "incorrect");
-            return null;
         }
-
         return loginMember;
     }
-
-    public void updateMember(String member_id, MemberDTO memberDTO) {
-        memberMapper.updateMember(member_id, memberDTO);
+    public void validateJoinInputs(MemberJoinDTO memberJoinDTO, BindingResult bindingResult) {
+        if (!StringUtils.hasText(memberJoinDTO.getMember_id())) {
+            log.error("member_id is empty");
+            bindingResult.rejectValue("member_id", "required");
+        }
+        if (!StringUtils.hasText(memberJoinDTO.getMember_pwd())) {
+            log.error("member_pwd is empty");
+            bindingResult.rejectValue("member_pwd", "required");
+        }
+        if (!StringUtils.hasText(memberJoinDTO.getMember_name())) {
+            log.error("member_name is empty");
+            bindingResult.rejectValue("member_name", "required");
+        }
+        if (!StringUtils.hasText(memberJoinDTO.getMember_email())) {
+            log.error("member_email is empty");
+            bindingResult.rejectValue("member_email", "required");
+        }
+        if (!StringUtils.hasText(memberJoinDTO.getMember_phone())) {
+            log.error("member_phone is empty");
+            bindingResult.rejectValue("member_phone", "required");
+        }
     }
-
 }
