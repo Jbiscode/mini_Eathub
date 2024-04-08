0a1bb391723309a8211352d5633b4bbdf6e9489e
maeng <soon5796735@naver.com>
2024-04-08 14:32:32 +0900

    ✨ FEAT : index 메인페이지 section4 레스토랑 리스트 db 연동 - section4 레스토랑 리스트 db 연동 - 제목, 별점, 카테고리 연동 - 각 항목 클릭 시 해당 레스토랑 페이지로 이동

--- src/main/java/com/eathub/controller/MainController.java
+++ src/main/java/com/eathub/controller/MainController.java
@@ -1,14 +1,28 @@
 package com.eathub.controller;
 
+import com.eathub.conf.SessionConf;
+import com.eathub.dto.SearchResultDTO;
+import com.eathub.dto.TimeOptionDTO;
+import com.eathub.service.RestaurantService;
+import com.eathub.service.SearchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final RestaurantService restaurantService;
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("page", "home");
+    public String index(Model model, HttpSession session) {
+        Long member_seq = (Long) session.getAttribute(SessionConf.LOGIN_MEMBER_SEQ);
+        List<SearchResultDTO> searchResultList = restaurantService.selectSearchResultList(member_seq);
+        model.addAttribute("restaurantList", searchResultList);
         return "index";
     }
 }
